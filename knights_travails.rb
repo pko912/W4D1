6e43759b2b7c65b4e8facb503e716546b9ef6405
“sajeremy” <9jsantiago4@gmail.com>
2022-10-03 15:08:11 -0400

    knights

--- knights_travails.rb
+++ knights_travails.rb
@@ -2,11 +2,21 @@ require_relative 'polytreenode'
 
 class KnightPathFinder
 
-    def initialize(start_pos)
-        @root_node = PolyTreeNode.new(start_pos)
+    def self.valid_moves(pos)
+        @considered_positions = [pos]
     end
 
-    
+    def initialize(start_pos)
+        @root_node = PolyTreeNode.new(start_pos)
+        
+    end
+
+    def build_move_tree
+    end
+
+    def new_move_positions(pos)
+        KnightPathFinder.valid_moves(pos)
+    end
 
 
 
