9521b25ec61f1ff183e210086aaaa41bced91283
“sajeremy” <9jsantiago4@gmail.com>
2022-10-03 16:38:56 -0400

    valid moves complete

--- knights_travails.rb
+++ knights_travails.rb
@@ -4,15 +4,25 @@ require 'byebug'
 class KnightPathFinder
 
     def self.valid_moves(pos)
-        relative_arr = [[-2,-1],[-2,1],[-1,-2],[1,2],[1,-2],[1,2],[2,-1],[2,1]]
-        debugger
-        valid_moves = relative_arr.map {|moves| moves+pos}
-        
-        filtered_moves = valid_moves.select do |moves|
-            
-            (moves[0] > 0 && moves[0] < 7) && (moves[1] > 0 && moves[1] < 7) 
-        end 
+        relative_arr = [[-2,-1],[-2,1],[-1,-2],[-1,2],[1,-2],[1,2],[2,-1],[2,1]]
+        valid_moves = []
+        relative_arr.each do |moves| 
+            x = moves[0] + pos[0]
+            y = moves[1] + pos[1]
+            valid_moves << [x,y]
+        end
+
+        filtered_moves = []
+        valid_moves.each do |moves|
+            x = moves[0]
+            y = moves[1]
+
+            if !((x < 0 || x > 7) || (y < 0 || y > 7))
+                filtered_moves << moves
+            end
+        end
         filtered_moves
+
     end
 
     def initialize(start_pos)
@@ -37,4 +47,4 @@ class KnightPathFinder
 end
 
 # p k = KnightPathFinder.new([0,0])
-p KnightPathFinder.valid_moves([3,0])
+p KnightPathFinder.valid_moves([4,4])
