25c38d6d60f19a07015feaafa7f44a3cb54fb53c
“sajeremy” <9jsantiago4@gmail.com>
2022-10-03 16:04:12 -0400

    move array

--- knights_travails.rb
+++ knights_travails.rb
@@ -3,11 +3,18 @@ require_relative 'polytreenode'
 class KnightPathFinder
 
     def self.valid_moves(pos)
-        @considered_positions = [pos]
+        relative_arr = [[-2,-1],[-2,1],[-1,-2],[1,2],[1,-2],[1,2],[2,-1],[2,1]]
+        valid_moves = relative_arr.map {|moves| moves+pos}
+        valid_moves.select do |arr|
+
+            (moves[0] > 0 && moves[0] < 7) && (moves[1] > 0 && moves[1] < 7) 
+            
+
     end
 
     def initialize(start_pos)
         @root_node = PolyTreeNode.new(start_pos)
+        @considered_positions = [start_pos]
         
     end
 
